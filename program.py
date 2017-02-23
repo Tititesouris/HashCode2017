a33779328ec3561c3c2d0b2641581fed68b787ef
Tititesouris <tititesouris@laposte.net>
2017-02-23 19:52:38 +0100

    Score of 0

--- program.py
+++ program.py
@@ -13,39 +13,17 @@ outputs = {}
 
 # Calculates the output from the input
 def getOutput(input):
-    # TODO calculate an output data from the input data
-    output=input
+    output = {}
+    nbCaches = input["cache_count"]
+    for cacheId in range(nbCaches):
+        output[cacheId] = []
+    '''for endpoint in input["endpoints"]:
+        for videoId, amountReq in endpoint["requests"]:
+            for cacheId in range(nbCaches):
+                output[cacheId].append(videoId)'''
     return output
-
 
 for name, input in inputs.items():
     outputs[name] = getOutput(input)
 
-#parseOutput.parseOutputs(outputs)
-
-data = {
-'videos': [50, 50, 80, 30, 110],
-'endpoint': [
-{
-'latency_to_dc': 1000,
-'latency_to_caches': {
-0: 100,
-1: 300,
-2: 200
-},
-'requests':
- { 1: 1000, 3: 1500, 4: 500 }
-
-},
-{
-'latency_to_dc': 500,
-'latency_to_caches': {},
-'requests':
- { 0: 1000 }
-}
-],
-'cache_size': 3,
-'cache_count': 100
-}
-
-print(getOutput(data))
+parseOutput.parseOutputs(outputs)
