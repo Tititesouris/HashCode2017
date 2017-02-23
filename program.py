06e29146e676a33f19667cecd9ec768b98388364
Phredd0 <jlwellsted@gmail.com>
2017-02-23 18:37:40 +0000

    Fixing titty's typos

--- program.py
+++ program.py
@@ -14,10 +14,38 @@ outputs = {}
 # Calculates the output from the input
 def getOutput(input):
     # TODO calculate an output data from the input data
-    return input
+    output=input
+    return output
 
 
 for name, input in inputs.items():
     outputs[name] = getOutput(input)
 
-parseOutput.parseOutputs(outputs)
+#parseOutput.parseOutputs(outputs)
+
+data = {
+'videos': [50, 50, 80, 30, 110],
+'endpoint': [
+{
+'latency_to_dc': 1000,
+'latency_to_caches': {
+0: 100,
+1: 300,
+2: 200
+},
+'requests':
+ { 1: 1000, 3: 1500, 4: 500 }
+
+},
+{
+'latency_to_dc': 500,
+'latency_to_caches': {},
+'requests':
+ { 0: 1000 }
+}
+],
+'cache_size': 3,
+'cache_count': 100
+}
+
+print(getOutput(data))
