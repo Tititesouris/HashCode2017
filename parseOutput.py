50192ecf61547ad4c3028203c5803c5baf5830e6
Tititesouris <tititesouris@laposte.net>
2017-02-23 19:11:25 +0100

    Parse output data into file

--- parseOutput.py
+++ parseOutput.py
@@ -3,8 +3,14 @@ outputPath = "output/"
 
 # Takes in data and returns a string representing that data
 def parseOutput(output):
-    # TODO transform output data into string
-    return "BOB"
+    nbCacheServers = len(output)
+    string = str(nbCacheServers) + "\n"
+    for cacheServer, videos in output.items():
+        string += str(cacheServer)
+        for video in videos:
+            string += " " + str(video)
+        string += "\n"
+    return string
 
 
 def parseOutputs(outputs):
